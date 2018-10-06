8e2c59a81734d1183b6773d8818448a39191b7fe
dobraga <douglasmartinsbraga@gmail.com>
2018-10-06 03:19:29 -0300

    Dash

--- dash_friends.R
+++ dash_friends.R
@@ -10,26 +10,52 @@ sidebar = dashboardSidebar(
               max = 10, value = c(1,10)),
   
   sidebarMenu(
-    menuItem("Exploratory", tabName = "exploratory", icon = icon("bar-chart")),
-    menuItem("Models", tabName = "models", icon = icon("cogs"))
+    menuItem("Exploratory", 
+             tabName = "exploratory", 
+             icon = icon("bar-chart"),
+                menuSubItem("Open exploratory",
+                            tabName = "exploratory_2",
+                            icon = NULL),
+             
+                menuSubItem(icon = NULL,
+                             infoBoxOutput("myscene_per")),
+             
+                menuSubItem(icon=NULL,
+                            selectInput("select_per", label = "Selecione o personagem", 
+                                        choices = c("Todos",colnames(scenes.adj[,-c(1,2)])), 
+                                        selected = 1))
+                ),
+    
+    menuItem("Models", 
+             tabName = "models", 
+             icon = icon("cogs"))
   ),
   
   wellPanel(
     tags$h4(style="color:black","Informações"),
     tags$div(style="color:black",
       tags$h6("Os dados utilizados foram extraídos da página: "),
-      tags$a(style="color:blue",href="https://fangj.github.io/friends/", "https://fangj.github.io/friends/")
+      tags$a(style="color:blue",href="https://fangj.github.io/friends/", "https://fangj.github.io/friends/"),
+      tags$h6("Alguns episódios estão com uma formatação diferente que não possui a marcação de cena e existem tambem episódios duplos, estes problemas explicam os saltos no histograma.")
     )
   )
 )
 
 body = dashboardBody(tabItems(
   # First tab content
-  tabItem(tabName = "exploratory",
+  tabItem(tabName = "exploratory_2",
           fluidRow(
-              valueBoxOutput("myscene"),
-              valueBoxOutput("qtdper")
-              )
+              infoBoxOutput("qtdses"),
+              infoBoxOutput("myscene"),
+              infoBoxOutput("qtdper")
+              ),
+          
+          fluidRow(box(title = "Distribuição de cenas por episódio e temporadas",
+                       plotOutput("histplot"),width = 6,height = 500),
+                   box(title = "Network",forceNetworkOutput("network"),width = 6,height = 500))#,
+          
+          #fluidRow()
+
           ),
   
   # Second tab content
@@ -49,20 +75,73 @@ server <- function(input, output) {
       filter(season >= input$season[1] , season <= input$season[2])
       })
   
-  output$myscene = renderValueBox({
-    valueBox(
+  dataset_per = reactive({
+    if(input$select_per != "Todos"){
+      scenes.adj %>% 
+        filter(scenes.adj[,colnames(scenes.adj) %in% input$select_per,drop=F]==T) %>% 
+        filter(season >= input$season[1] , season <= input$season[2])
+    }
+  })
+  
+  output$qtdses = renderInfoBox({
+    infoBox(
+      "Quantidade de temporadas",
+      value = length(unique(dataset()$season)),
+      icon = icon("bookmark")
+    )
+  })
+  
+  output$myscene = renderInfoBox({
+    infoBox(
+      "Total de cenas",
       value = nrow(dataset()),
-      subtitle = "Total de cenas",
       icon = icon("film")
     )
   })
   
-  output$qtdper = renderValueBox({
-    valueBox(
-      value = sum(apply(dataset(),2,max) != 0),
-      subtitle = "Total de personagens",
+  output$myscene_per = renderInfoBox({
+    if(input$select_per == "Todos"){
+      infoBox(
+        "Total de cenas",
+        value = nrow(dataset()),
+        icon = icon("film")
+      )
+    }else{
+      infoBox(
+        "Total de cenas do personagem",
+        value = nrow(dataset_per()),
+        icon = icon("film")
+      )
+    }
+  })
+  
+  output$qtdper = renderInfoBox({
+    infoBox(
+      "Total de personagens",
+      value = sum(apply(dataset()[,-c(1,2)],2,max) != 0),
       icon = icon("users")
     )
+  })
+  
+  output$histplot = renderPlot({
+    if(input$select_per == "Todos"){
+      ggplot() + 
+      #geom_bar(data = scenes.adj,aes(x=episode),fill = "grey85") + 
+      geom_bar(data = dataset(),aes(x=episode)) +
+      labs(x="Episódio", y="Quantidade de cenas") +
+      facet_wrap(~season,scales = "free")
+    }else{
+      ggplot() + 
+      #geom_bar(data = scenes.adj,aes(x=episode),fill = "grey85") + 
+      geom_bar(data = dataset(),aes(x=episode)) +
+      geom_bar(data = dataset_per(),aes(x=episode),fill='red',alpha=.7) +
+      labs(x="Episódio", y="Quantidade de cenas") +
+      facet_wrap(~season,scales = "free")
+    }
+  })
+  
+  output$network = renderForceNetwork({
+    view_net(cria.socio.matriz(dataset()))
   })
   
 }
